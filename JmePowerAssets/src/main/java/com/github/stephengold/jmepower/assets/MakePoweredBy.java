81aabb467f455b96451608f2c83a8dc97c08a488
Stephen Gold <sgold@sonic.net>
2022-11-09 07:58:00 -0800

    MakePoweredBy: finalize the class

--- JmePowerAssets/src/main/java/com/github/stephengold/jmepower/assets/MakePoweredBy.java
+++ JmePowerAssets/src/main/java/com/github/stephengold/jmepower/assets/MakePoweredBy.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2020-2021, Stephen Gold
+ Copyright (c) 2020-2022, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -39,7 +39,7 @@ import jme3utilities.MyString;
  *
  * @author Stephen Gold sgold@sonic.net
  */
-public class MakePoweredBy extends MakeSquareTexture {
+final public class MakePoweredBy extends MakeSquareTexture {
     // *************************************************************************
     // constants and loggers
 
