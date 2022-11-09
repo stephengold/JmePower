9dcafdb74261e84b3fffc0893d5b404a07d5d9b8
Stephen Gold <sgold@sonic.net>
2022-11-08 17:58:47 -0800

    Preloader:  publicize the logger

--- JmePowerLibrary/src/main/java/com/github/stephengold/jmepower/Preloader.java
+++ JmePowerLibrary/src/main/java/com/github/stephengold/jmepower/Preloader.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2020-2021, Stephen Gold
+ Copyright (c) 2020-2022, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -44,7 +44,7 @@ public class Preloader extends Thread {
     /**
      * message logger for this class
      */
-    final private static Logger logger
+    final public static Logger logger
             = Logger.getLogger(Preloader.class.getName());
     // *************************************************************************
     // fields
