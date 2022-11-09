b4dc70f391ecec6fb58ccd2c040180a6a9320f80
Stephen Gold <sgold@sonic.net>
2022-11-08 17:30:04 -0800

    add the JmePowerExamples sub-project

---
+++ JmePowerExamples/src/main/java/mygame/HelloJmePower.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2009-2012 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package mygame;
+
+import com.github.stephengold.jmepower.JmeLoadingState;
+import com.jme3.app.DebugKeysAppState;
+import com.jme3.app.FlyCamAppState;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.StatsAppState;
+import com.jme3.app.state.AppState;
+import com.jme3.asset.AssetKey;
+import com.jme3.asset.AssetManager;
+import com.jme3.cinematic.events.AnimEvent;
+import com.jme3.material.Material;
+import com.jme3.material.MaterialDef;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import java.util.logging.Level;
+import jme3utilities.Loadable;
+
+/**
+ * This is the HelloJME3 application from jme3-examples ... with JmePower added.
+ */
+public class HelloJmePower extends SimpleApplication {
+
+    public HelloJmePower() {
+        super((AppState[]) null);
+    }
+
+    public static void main(String[] args) {
+        HelloJmePower app = new HelloJmePower();
+        app.start(); // start the game
+    }
+
+    @Override
+    public void simpleInitApp() {
+        AnimEvent.logger.setLevel(Level.WARNING);
+        JmeLoadingState loading = new JmeLoadingState(
+                (Loadable) (AssetManager am) -> {
+                    AssetKey<MaterialDef> key = new AssetKey<>("Common/MatDefs/Misc/Unshaded.j3md");
+                    am.loadAsset(key);
+                    try {
+                        Thread.sleep(1000L);
+                    } catch (InterruptedException exception) {
+                    }
+                }
+        );
+        stateManager.attach(loading);
+    }
+
+    private void moreInit() {
+        stateManager.attachAll(
+                new DebugKeysAppState(),
+                new FlyCamAppState(),
+                new StatsAppState()
+        );
+
+        cam.setLocation(new Vector3f(0f, 0f, 10f));
+
+        Box b = new Box(1, 1, 1); // create cube shape
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
+        Material mat = new Material(assetManager,
+                "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        mat.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
+        geom.setMaterial(mat);                   // set the cube's material
+        rootNode.attachChild(geom);              // make the cube appear in the scene
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        AppState loading = stateManager.getState(JmeLoadingState.class);
+        if (loading != null && !loading.isEnabled()) {
+            getStateManager().detach(loading);
+            moreInit();
+        }
+    }
+}
